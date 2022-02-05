a3c87c22c8fa507f79c7489a054b3af221919d92
75009620 <omer.bulut@partner.turktelekom.com.tr>
2022-02-05 15:19:34 +0300

    postgresql example project is created.

---
+++ projectpostgre/src/test/java/com/example/projectpostgre/ProjectpostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectpostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectpostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
